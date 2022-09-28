0a167acf86bb909cddba5a75e35388cc4cbae777
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2022-09-28 14:17:18 +0530

    Add files via upload

---
+++ text-app/src/test/java/com/example/textapp/TextAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.textapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
